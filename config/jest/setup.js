40f8217113f948dd6a0cb884acf3014baf885020
dprovodnikov <dp.wireden@gmail.com>
2019-01-29 16:15:54 +0200

    Set up testing environemnt with jest & enzyme

---
+++ config/jest/setup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+global.React = React;
+global.shallow = shallow;
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
