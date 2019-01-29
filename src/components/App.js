5f4a80e30006d226e054427afbd1cc5e3ebfa9ac
dprovodnikov <dp.wireden@gmail.com>
2019-01-29 20:33:05 +0200

    Set up react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = ({ store }) => (
+const App = ({ store, history }) => (
   <Provider store={store}>
-    <div>
-      Hello, world!
-    </div>
+    <Router history={history}>
+      <div>
+        Hello, world!
+      </div>
+    </Router>
   </Provider>
 );
 
 App.propTypes = {
   store: PropTypes.shape().isRequired,
+  history: PropTypes.shape().isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import configureStore from './modules/store';
 import services from './services';
 
 const markup = (
-  <App store={configureStore(services)} />
+  <App
+    history={createHistory()}
+    store={configureStore(services)}
+  />
 );
 
 ReactDOM.render(markup, document.getElementById('root'));
