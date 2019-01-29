63aee3bd1128b38aacabce706f9169c2bf9f7c69
dprovodnikov <dp.wireden@gmail.com>
2019-01-29 14:37:28 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => (
+  <div>
+    Hello, world!
+  </div>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ webpack.development.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const BUILD_FOLDER = 'build';
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    bundle: './src/index.js',
+  },
+
+  output: {
+    filename: '[name].[hash].bundle.js',
+    path: path.resolve(__dirname, '..', BUILD_FOLDER),
+    publicPath: '/',
+  },
+
+  devtool: 'inline-source-map',
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '../src'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: 'url-loader?limit=30000&name=[path][name].[ext]',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      title: 'Identifo Admin Panel',
+      template: path.resolve(__dirname, './template.html'),
+    }),
+    new CleanPlugin([BUILD_FOLDER], {
+      root: path.resolve(__dirname, '..'),
+    }),
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, '/'),
+    port: 3000,
+  },
+};
+
