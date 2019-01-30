bee589b0f277c0c016262f90c2e7150d9f80675c
dprovodnikov <dp.wireden@gmail.com>
2019-01-30 13:28:50 +0200

    Add actions for auth module

---
+++ src/modules/auth/actions.js
@@ -0,0 +1,31 @@
+import actionCreator from '@madappgang/action-creator';
+import types from './types';
+
+const loginAttempt = actionCreator(types.LOGIN_ATTEMPT);
+const loginSuccess = actionCreator(types.LOGIN_SUCCESS);
+const loginFailure = actionCreator(types.LOGIN_FAILURE);
+
+const logoutAttempt = actionCreator(types.LOGOUT_ATTEMPT);
+const logoutSuccess = actionCreator(types.LOGOUT_SUCCESS);
+
+const login = (email, password) => async (dispatch, _, { auth }) => {
+  dispatch(loginAttempt());
+
+  try {
+    await auth.login(email, password);
+    dispatch(loginSuccess());
+  } catch (err) {
+    dispatch(loginFailure(err));
+  }
+};
+
+const logout = () => async (dispatch, _, { auth }) => {
+  dispatch(logoutAttempt());
+  await auth.logout();
+  dispatch(logoutSuccess());
+};
+
+export {
+  login,
+  logout,
+};
