1a6bbcfd011c13f645fa46df38f449c13083ebc4
dprovodnikov <dp.wireden@gmail.com>
2019-02-02 01:00:56 +0200

    Change the main color of the application

--- src/components/screens/Management/Header/index.js
+++ src/components/screens/Management/Header/index.js
@@ -6,8 +6,10 @@ const ManagementScreenHeader = () => {
   return (
     <header className="iap-management-header">
       <Container>
-        <div className="iap-management-header__logo">
-          Identifo
+        <div className="iap-management-header__valign">
+          <span className="iap-management-header__logo">
+            Identifo
+          </span>
         </div>
       </Container>
     </header>
