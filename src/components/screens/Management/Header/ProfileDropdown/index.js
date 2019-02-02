8b97166c98e39ed55c1e11555883d4d51b8bd3c0
dprovodnikov <dp.wireden@gmail.com>
2019-02-02 17:52:28 +0200

    Extract dropdown functionality into a generic dropdown component

--- src/components/screens/Management/Header/ProfileDropdown/index.js
+++ src/components/screens/Management/Header/ProfileDropdown/index.js
@@ -1,66 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DropdownIcon from '~/components/shared/icons/DropdownIcon';
 import LogoutSection from './LogoutSection';
+import Dropdown from '~/components/shared/Dropdown';
 import './Profile.css';
 
-class ProfileDropdown extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      open: false,
-    };
-
-    this.open = this.open.bind(this);
-    this.close = this.close.bind(this);
-    this.handleGlobalClick = this.handleGlobalClick.bind(this);
-  }
-
-  componentDidMount() {
-    document.addEventListener('click', this.handleGlobalClick);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('click', this.handleGlobalClick);
-  }
-
-  handleGlobalClick({ target }) {
-    if (this.ref.contains(target)) {
-      return;
-    }
-
-    this.close();
-  }
-
-  open() {
-    this.setState({ open: true });
-  }
-
-  close() {
-    this.setState({ open: false });
-  }
-
-  render() {
-    const { open } = this.state;
-
-    return (
-      <div className="iap-header-profile" ref={el => this.ref = el}>
-        <button
-          type="button"
-          className="iap-header-profile__trigger"
-          onClick={this.open}
-        >
-          <span>Admin Panel</span>
-          <DropdownIcon />
-        </button>
-        {open && (
-          <div className="iap-profile-dropdown">
-            <LogoutSection />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const ProfileDropdown = () => {
+  return (
+    <Dropdown>
+      {({ isOpen, open }) => (
+        <div className="iap-header-profile">
+          <button
+            type="button"
+            className="iap-header-profile__trigger"
+            onClick={open}
+          >
+            <span>Admin Panel</span>
+            <DropdownIcon />
+          </button>
+          {isOpen && (
+            <div className="iap-profile-dropdown">
+              <LogoutSection />
+            </div>
+          )}
+        </div>
+      )}
+    </Dropdown>
+  );
+};
 
 export default ProfileDropdown;
---
+++ src/components/shared/Dropdown/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Dropdown extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      open: false,
+    };
+
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+    this.handleGlobalClick = this.handleGlobalClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleGlobalClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleGlobalClick);
+  }
+
+  handleGlobalClick({ target }) {
+    if (this.ref.contains(target)) {
+      return;
+    }
+
+    this.close();
+  }
+
+  open() {
+    this.setState({ open: true });
+  }
+
+  close() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    const { children } = this.props;
+
+    if (!children) {
+      return null;
+    }
+
+    return (
+      <div ref={el => this.ref = el}>
+        {children({
+          open: this.open,
+          close: this.close,
+          isOpen: this.state.open,
+        })}
+      </div>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default Dropdown;
