f84d360a9181beeb7bf6e96c51860bb368e1bf9c
dprovodnikov <dp.wireden@gmail.com>
2019-02-02 23:41:32 +0200

    Add appearance for the "admin account" area of "settings" section

--- src/components/screens/Management/sections/Settings/index.js
+++ src/components/screens/Management/sections/Settings/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Settings.css';
 
 const SettingsSection = () => {
   return (
@@ -6,6 +7,31 @@ const SettingsSection = () => {
       <p className="iap-management-section__title">
         Settings
       </p>
+
+      <div className="iap-settings-section">
+        <span className="iap-settings-section__title">
+          Admin account
+        </span>
+
+        <p className="iap-settings-section__description">
+          These are credentials used to login into admin panel.
+        </p>
+
+        <div className="iap-settings-section__info">
+          <span>Email</span>
+          <p className="iap-settings-section__email">
+            admin@company.com
+          </p>
+        </div>
+
+        <button
+          type="button"
+          className="iap-settings-section__update-btn"
+        >
+          Change
+        </button>
+      </div>
+
     </section>
   );
 };
