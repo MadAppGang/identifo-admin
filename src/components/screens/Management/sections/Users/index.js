9105f883e5bc20cc3e8051ba9e1ede5e168f4985
dprovodnikov <dp.wireden@gmail.com>
2019-02-03 12:30:30 +0200

    Add users section placeholder view component

--- src/components/screens/Management/sections/Users/index.js
+++ src/components/screens/Management/sections/Users/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import usersIcon from './user.svg';
+import plusIcon from './plus-white.svg';
+import Button from '~/components/shared/Button';
 import './UsersSection.css';
 
 const UsersSection = () => {
   return (
     <section className="iap-management-section">
-      <p className="iap-management-section__title">
-        Users
-      </p>
+      <div className="iap-users-placehoder">
+        <h2 className="iap-users-placeholder__title">
+          Users
+        </h2>
+
+        <img src={usersIcon} alt="users" className="iap-users-placeholder__icon" />
+
+        <p className="iap-users-placeholder__msg">
+          No users have been added to your applications.
+        </p>
+
+        <Button>
+          <img
+            src={plusIcon}
+            alt="add"
+            className="iap-users-placeholder__add-icon"
+          />
+          <span className="iap-users-placeholder__btn-text">
+            Create your first user
+          </span>
+        </Button>
+      </div>
     </section>
   );
 };
---
+++ src/components/shared/Button/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = props => (
+  <button
+    type={props.type}
+    className={'iap-btn'.concat(props.stretch ? ' iap-btn--stretch' : '')}
+    disabled={props.disabled}
+    onClick={props.onClick}
+  >
+    {props.children}
+  </button>
+);
+
+Button.propTypes = {
+  type: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  stretch: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  onClick: null,
+  disabled: false,
+  stretch: false,
+};
+
+export default Button;
