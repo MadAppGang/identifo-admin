57193f7fc148e324877a76e1d94fe5d7f087342d
dprovodnikov <dp.wireden@gmail.com>
2019-02-03 13:02:48 +0200

    Extract users placeholder into a separate component

---
+++ src/components/screens/Management/sections/Users/Placeholder/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import usersIcon from './user.svg';
+import plusIcon from './plus-white.svg';
+import Button from '~/components/shared/Button';
+import './UsersPlaceholder.css';
+
+const UsersPlaceholder = () => {
+  return (
+    <div className="iap-users-placehoder">
+      <h2 className="iap-users-placeholder__title">
+        Users
+      </h2>
+
+      <img src={usersIcon} alt="users" className="iap-users-placeholder__icon" />
+
+      <p className="iap-users-placeholder__msg">
+        No users have been added to your applications.
+      </p>
+
+      <Button>
+        <img
+          src={plusIcon}
+          alt="add"
+          className="iap-users-placeholder__add-icon"
+        />
+        <span className="iap-users-placeholder__btn-text">
+          Create your first user
+        </span>
+      </Button>
+    </div>
+  );
+};
+
+export default UsersPlaceholder;
