fb9d74c6bb9f8d0eb4dd85ad037408973b90b11b
dprovodnikov <dp.wireden@gmail.com>
2019-02-10 21:05:23 +0200

    Add account module reducer

---
+++ src/modules/account/reducer.js
@@ -0,0 +1,32 @@
+import update from '@madappgang/update-by-path';
+import types from './types';
+
+const INITIAL_STATE = {
+  fetching: false,
+  posting: false,
+  settings: null,
+  error: null,
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case types.FETCH_ACCOUNT_SETTINGS_ATTEMPT:
+      return update(state, { fetching: true });
+    case types.FETCH_ACCOUNT_SETTINGS_SUCCESS:
+      return update(state, { fetching: false, settings: payload });
+    case types.FETCH_ACCOUNT_SETTINGS_FAILURE:
+      return update(state, { fetching: false, error: payload });
+    case types.POST_ACCOUNT_SETTINGS_ATTEMPT:
+      return update(state, { posting: true });
+    case types.POST_ACCOUNT_SETTINGS_SUCCESS:
+      return update(state, { posting: false });
+    case types.POST_ACCOUNT_SETTINGS_FAILURE:
+      return update(state, { posting: false, error: payload });
+    default:
+      return state;
+  }
+};
+
+export default reducer;
