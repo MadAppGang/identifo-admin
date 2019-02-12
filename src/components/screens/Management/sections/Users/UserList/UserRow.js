86ad897eff9b5c4bc4856120dd21c00bc3b31e6a
dprovodnikov <dp.wireden@gmail.com>
2019-02-12 12:36:11 +0200

    Implement users datagrid basic view (set of components, styles)

---
+++ src/components/screens/Management/sections/Users/UserList/UserRow.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { DatagridRow } from '~/components/shared/Datagrid';
+
+const getInitials = (name) => {
+  // TODO: poor implementation
+  const [first, last] = name.toUpperCase().split(/\s/);
+  return `${first.slice(0, 1)}${last.slice(0, 1)}`;
+};
+
+const UserRow = ({ data, config }) => (
+  <DatagridRow>
+    <div style={{ width: config.icon.width }}>
+      <div className="iap-datagrid-row__user-icon">
+        {getInitials(data.name)}
+      </div>
+    </div>
+    <div style={{ width: config.name.width }}>
+      {data.name}
+    </div>
+    <div style={{ width: config.email.width }}>
+      {data.email}
+    </div>
+    <div style={{ width: config.latestLogin.width }}>
+      {data.latestLogin}
+    </div>
+    <div style={{ width: config.numberOfLogins.width }}>
+      {data.numberOfLogins}
+    </div>
+  </DatagridRow>
+);
+
+UserRow.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    latestLogin: PropTypes.string,
+    numberOfLogins: PropTypes.string,
+  }).isRequired,
+  config: PropTypes.shape({
+    name: PropTypes.object,
+    email: PropTypes.object,
+    latestLogin: PropTypes.object,
+    numberOfLogins: PropTypes.object,
+  }).isRequired,
+};
+
+export default UserRow;
--- src/components/screens/Management/sections/Users/index.js
+++ src/components/screens/Management/sections/Users/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UsersPlaceholder from './Placeholder';
+import UserList from './UserList';
 
-const UsersSection = () => {
-  return (
-    <section className="iap-management-section">
-      <UsersPlaceholder />
-    </section>
-  );
+class UsersSection extends Component {
+  componendDidMount() {
+    this.props.fetchUsers();
+  }
+
+  render() {
+    const { users } = this.props;
+
+    if (!users || !users.length) {
+      return (
+        <section className="iap-management-section">
+          <UsersPlaceholder />
+        </section>
+      );
+    }
+
+    return (
+      <section className="iap-management-section">
+        <p className="iap-management-section__title">
+          Users
+        </p>
+
+        <UserList users={users} />
+      </section>
+    );
+  }
+}
+
+UsersSection.defaultProps = {
+  users: [
+    {
+      id: '1',
+      name: 'Denys Provodnikov',
+      email: 'dp@madappgang.com',
+      latestLogin: 'Never',
+      numberOfLogins: 0,
+    },
+    {
+      id: '2',
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      latestLogin: 'Yesterday',
+      numberOfLogins: 3,
+    },
+    {
+      id: '3',
+      name: 'Falangus Minelly',
+      email: 'angus.fm@gmail.com',
+      latestLogin: '2 hours ago',
+      numberOfLogins: 14,
+    },
+  ],
+  fetchUsers: () => {},
 };
 
 export default UsersSection;
