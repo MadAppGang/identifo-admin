04c4ec7dec132800988fe1aafe12c6706f63d871
dprovodnikov <dp.wireden@gmail.com>
2019-02-14 22:20:50 +0200

    Add unit tests for users reducer covering "postUser" functionality

--- src/modules/users/__tests__/reducer.test.js
+++ src/modules/users/__tests__/reducer.test.js
@@ -28,4 +28,41 @@ describe('users module reducer', () => {
     const action = { type: types.FETCH_USERS_FAILURE, payload: err };
     expect(reducer(undefined, action).error).toBe(err);
   });
+
+  test('sets posting to true on post attempt', () => {
+    const action = { type: types.POST_USER_ATTEMPT };
+    expect(reducer(undefined, action).posting).toBe(true);
+  });
+
+  test('sets posting to false on post success', () => {
+    const action = { type: types.POST_USER_SUCCESS };
+    expect(reducer(undefined, action).posting).toBe(false);
+  });
+
+  test('sets posting to false on post failure', () => {
+    const action = { type: types.POST_USER_FAILURE };
+    expect(reducer(undefined, action).posting).toBe(false);
+  });
+
+  test('sets payload to the end of the list on post success', () => {
+    const user = {};
+    const action = { type: types.POST_USER_SUCCESS, payload: user };
+    const state = {
+      posting: true,
+      list: [],
+      error: null,
+    };
+    const expectedState = {
+      posting: false,
+      list: [user],
+      error: null,
+    };
+    expect(reducer(state, action)).toEqual(expectedState);
+  });
+
+  test('sets error to payload on post failure', () => {
+    const err = new Error('error');
+    const action = { type: types.POST_USER_FAILURE, payload: err };
+    expect(reducer(undefined, action).error).toBe(err);
+  });
 });
