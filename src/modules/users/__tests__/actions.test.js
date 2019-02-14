8f9ad219b445be69f44d8c5f2cecc57bcb5a5c21
dprovodnikov <dp.wireden@gmail.com>
2019-02-14 22:38:51 +0200

    Add unit tests for postUser redux action-creator

--- src/modules/users/__tests__/actions.test.js
+++ src/modules/users/__tests__/actions.test.js
@@ -1,4 +1,4 @@
-import { fetchUsers } from '../actions';
+import { fetchUsers, postUser } from '../actions';
 import types from '../types';
 
 describe('fetchUsers redux action', () => {
@@ -48,5 +48,47 @@ describe('fetchUsers redux action', () => {
     const filters = {};
     fetchUsers(filters)(dispatch, null, { users: userService });
     expect(userService.fetchUsers).toHaveBeenCalledWith(filters);
+  });
+});
+
+describe('postUser redux action creator', () => {
+  let dispatch;
+  let userService;
+
+  beforeEach(() => {
+    dispatch = jest.fn();
+    userService = {
+      postUser: jest.fn(),
+    };
+  });
+
+  test('calls dispatch with attempt action', () => {
+    const expectedAction = { type: types.POST_USER_ATTEMPT };
+    postUser({})(dispatch, null, { users: userService });
+    expect(dispatch).toBeCalledWith(expectedAction);
+  });
+
+  test('calls dispatch second time with success action', async () => {
+    const response = {};
+    const expectedAction = { type: types.POST_USER_SUCCESS, payload: response };
+
+    userService.postUser.mockReturnValue(Promise.resolve(response));
+    await postUser({})(dispatch, null, { users: userService });
+    expect(dispatch).toHaveBeenNthCalledWith(2, expectedAction);
+  });
+
+  test('calls dispatch second time with failure action on rejected service call', async () => {
+    const reason = new Error('reason');
+    const expectedAction = { type: types.POST_USER_FAILURE, payload: reason };
+
+    userService.postUser.mockReturnValue(Promise.reject(reason));
+    await postUser({})(dispatch, null, { users: userService });
+    expect(dispatch).toHaveBeenNthCalledWith(2, expectedAction);
+  });
+
+  test('invokes postUser service method', () => {
+    const user = {};
+    postUser(user)(dispatch, null, { users: userService });
+    expect(userService.postUser).toBeCalledWith(user);
   });
 });
