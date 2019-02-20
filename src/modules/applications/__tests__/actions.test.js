cff938305a2c55a9e52665dff7df82d19e1420f8
dprovodnikov <dp.wireden@gmail.com>
2019-02-20 08:16:31 +0200

    Add "postApplication" redux action creator

--- src/modules/applications/__tests__/actions.test.js
+++ src/modules/applications/__tests__/actions.test.js
@@ -1,4 +1,4 @@
-import { fetchApplications } from '../actions';
+import { fetchApplications, postApplication } from '../actions';
 import types from '../types';
 
 describe('[applications] fetchApplications action creator', () => {
@@ -37,5 +37,45 @@ describe('[applications] fetchApplications action creator', () => {
   test('invokes applications service to fetch the list', () => {
     fetchApplications()(dispatch, null, { applications });
     expect(applications.fetchApplications).toBeCalled();
+  });
+});
+
+describe('[applications] postApplication action creator', () => {
+  let dispatch;
+  let applications;
+
+  beforeEach(() => {
+    dispatch = jest.fn();
+    applications = {
+      postApplication: jest.fn(() => Promise.resolve({})),
+    };
+  });
+
+  test('calls dispatch with attempt action', () => {
+    const action = { type: types.POST_APPLICATION_ATTEMPT };
+    postApplication()(dispatch, null, { applications });
+    expect(dispatch).toBeCalledWith(action);
+  });
+
+  test('calls dispatch second time with success action', async () => {
+    const app = {};
+    const action = { type: types.POST_APPLICATION_SUCCESS, payload: app };
+    applications.postApplication.mockReturnValue(Promise.resolve(app));
+    await postApplication()(dispatch, null, { applications });
+    expect(dispatch).toHaveBeenNthCalledWith(2, action);
+  });
+
+  test('calls dispatch second time with failure action', async () => {
+    const reason = new Error('error');
+    const action = { type: types.POST_APPLICATION_FAILURE, payload: reason };
+    applications.postApplication.mockReturnValue(Promise.reject(reason));
+    await postApplication({})(dispatch, null, { applications });
+    expect(dispatch).toHaveBeenNthCalledWith(2, action);
+  });
+
+  test('invokes applications service to post an application', () => {
+    const app = {};
+    postApplication(app)(dispatch, null, { applications });
+    expect(applications.postApplication).toBeCalledWith(app);
   });
 });
--- src/modules/applications/actions.js
+++ src/modules/applications/actions.js
@@ -5,6 +5,10 @@ const fetchAttempt = actionCreator(types.FETCH_APPLICATIONS_ATTEMPT);
 const fetchSuccess = actionCreator(types.FETCH_APPLICATIONS_SUCCESS);
 const fetchFailure = actionCreator(types.FETCH_APPLICATIONS_FAILURE);
 
+const postAttempt = actionCreator(types.POST_APPLICATION_ATTEMPT);
+const postSuccess = actionCreator(types.POST_APPLICATION_SUCCESS);
+const postFailure = actionCreator(types.POST_APPLICATION_FAILURE);
+
 const fetchApplications = () => async (dispatch, _, { applications }) => {
   dispatch(fetchAttempt());
 
@@ -16,6 +20,18 @@ const fetchApplications = () => async (dispatch, _, { applications }) => {
   }
 };
 
+const postApplication = application => async (dispatch, _, { applications }) => {
+  dispatch(postAttempt());
+
+  try {
+    const result = await applications.postApplication(application);
+    dispatch(postSuccess(result));
+  } catch (err) {
+    dispatch(postFailure(err));
+  }
+};
+
 export {
   fetchApplications,
+  postApplication,
 };
