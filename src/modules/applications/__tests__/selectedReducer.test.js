e52179e6f46cb06ce99ccbdb3c007614dbb31e6c
dprovodnikov <dp.wireden@gmail.com>
2019-02-20 22:00:10 +0200

    Add unit tests for "delete" case of selected application reducer

--- src/modules/applications/__tests__/selectedReducer.test.js
+++ src/modules/applications/__tests__/selectedReducer.test.js
@@ -28,4 +28,25 @@ describe('[applications] selected reducer', () => {
     const action = { type: types.POST_APPLICATION_FAILURE, payload: err };
     expect(reducer({ error: null }, action).error).toEqual(err);
   });
+
+  test('sets saving to true on delete attempt', () => {
+    const action = { type: types.DELETE_APPLICATION_ATTEMPT };
+    expect(reducer({ saving: false }, action).saving).toBe(true);
+  });
+
+  test('sets saving to false on delete success', () => {
+    const action = { type: types.DELETE_APPLICATION_SUCCESS };
+    expect(reducer({ saving: true }, action).saving).toBe(false);
+  });
+
+  test('sets saving to false on delete failure', () => {
+    const action = { type: types.DELETE_APPLICATION_FAILURE };
+    expect(reducer({ saving: true }, action).saving).toBe(false);
+  });
+
+  test('sets error to payload on delete failure', () => {
+    const err = new Error('message');
+    const action = { type: types.DELETE_APPLICATION_FAILURE, payload: err };
+    expect(reducer({ error: null }, action).error).toEqual(err);
+  });
 });
