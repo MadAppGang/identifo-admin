2838d97b9f46f0cc3fe1b1e624a38bbe76cd02c4
dprovodnikov <dp.wireden@gmail.com>
2019-06-07 14:02:06 +0300

    Replace the use of Dropdown component with useDropdown hook from npm

--- src/components/shared/ActionsButton/index.js
+++ src/components/shared/ActionsButton/index.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useDropdown from 'use-dropdown';
 import Button from '~/components/shared/Button';
-import Dropdown from '~/components/shared/Dropdown';
 import DropdownIcon from '~/components/icons/DropdownIcon';
 import LoadingIcon from '~/components/icons/LoadingIcon';
 
 import './ActionsButton.css';
 
-const ActionsButton = props => (
-  <Dropdown>
-    {({ isOpen, open }) => (
-      <div className="iap-actions-btn">
-        <Button
-          Icon={props.loading ? LoadingIcon : DropdownIcon}
-          disabled={props.loading}
-          iconClassName="iap-actions-btn__icon"
-          onClick={open}
-        >
-          {props.text}
-        </Button>
-        {isOpen && (
-          <div className="iap-actions-btn__actions">
-            {props.actions.map((action) => {
-              return (
-                <button
-                  key={action.title}
-                  type="button"
-                  className="iap-actions-btn__action"
-                  onClick={action.onClick}
-                >
-                  {action.title}
-                </button>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    )}
-  </Dropdown>
-);
+const ActionsButton = (props) => {
+  const [containerRef, isOpen, open] = useDropdown();
+
+  return (
+    <div className="iap-actions-btn" ref={containerRef}>
+      <Button
+        Icon={props.loading ? LoadingIcon : DropdownIcon}
+        disabled={props.loading}
+        iconClassName="iap-actions-btn__icon"
+        onClick={open}
+      >
+        {props.text}
+      </Button>
+      {isOpen && (
+        <div className="iap-actions-btn__actions">
+          {props.actions.map((action) => {
+            return (
+              <button
+                key={action.title}
+                type="button"
+                className="iap-actions-btn__action"
+                onClick={action.onClick}
+              >
+                {action.title}
+              </button>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 ActionsButton.propTypes = {
   actions: PropTypes.arrayOf(PropTypes.shape({
