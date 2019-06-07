01f31b8574f3652518e17d5c32ae956ebeb64d2b
dprovodnikov <dp.wireden@gmail.com>
2019-06-07 13:57:17 +0300

    replace the use of Dropdown component with useDropdown hook in ProfileDropdown

--- src/components/screens/Management/Header/ProfileDropdown/index.js
+++ src/components/screens/Management/Header/ProfileDropdown/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
+import useDropdown from 'use-dropdown';
 import DropdownIcon from '~/components/icons/DropdownIcon';
 import LogoutSection from './LogoutSection';
 import AccountSection from './AccountSection';
-import Dropdown from '~/components/shared/Dropdown';
 import './Profile.css';
 
 const ProfileDropdown = () => {
+  const [containerRef, isOpen, open, close] = useDropdown();
+
   return (
-    <Dropdown>
-      {({ isOpen, open, close }) => (
-        <div className="iap-header-profile">
-          <button
-            type="button"
-            className="iap-header-profile__trigger"
-            onClick={open}
-          >
-            <span>Admin Panel</span>
-            <DropdownIcon className="iap-dropdown-icon" />
-          </button>
-          {isOpen && (
-            <div className="iap-profile-dropdown">
-              <AccountSection onClick={close} />
-              <LogoutSection onClick={close} />
-            </div>
-          )}
+    <div className="iap-header-profile" ref={containerRef}>
+      <button
+        type="button"
+        className="iap-header-profile__trigger"
+        onClick={open}
+      >
+        <span>Admin Panel</span>
+        <DropdownIcon className="iap-dropdown-icon" />
+      </button>
+      {isOpen && (
+        <div className="iap-profile-dropdown">
+          <AccountSection onClick={close} />
+          <LogoutSection onClick={close} />
         </div>
       )}
-    </Dropdown>
+    </div>
   );
 };
 
