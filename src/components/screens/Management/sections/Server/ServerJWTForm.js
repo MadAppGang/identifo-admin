ae117d1fc5ee5ed2d5e128cd17d1b91eb6bae68e
dprovodnikov <dp.wireden@gmail.com>
2019-08-22 20:43:29 +0300

    Add storage type to jwt settings tab. Add according type-specific fields as well.

--- src/components/screens/Management/sections/Server/ServerJWTForm.js
+++ src/components/screens/Management/sections/Server/ServerJWTForm.js
@@ -6,23 +6,44 @@ import Field from '~/components/shared/Field';
 import Button from '~/components/shared/Button';
 import SaveIcon from '~/components/icons/SaveIcon';
 import LoadingIcon from '~/components/icons/LoadingIcon';
+import { Select, Option } from '~/components/shared/Select';
+
+const storageTypes = {
+  FILE: 'file',
+  S3: 's3',
+};
+
+const keyDescription = {
+  [storageTypes.FILE]: 'Must be a path to file (e.g, ./jwt/public.pem)',
+  [storageTypes.S3]: 'Must be a name of an object in the bucket (e.g, public.pem)',
+};
 
 const ServerJWTForm = (props) => {
   const { error, loading, settings, onSubmit } = props;
 
-  const [publicKeyPath, setPublicKeyPath] = useState(settings ? settings.publicKeyPath : '');
-  const [privateKeyPath, setPrivateKeyPath] = useState(settings ? settings.privateKeyPath : '');
+  const [storageType, setStorageType] = useState(settings ? settings.type : '');
+  const [publicKey, setPublicKey] = useState(settings ? settings.publicKey : '');
+  const [privateKey, setPrivateKey] = useState(settings ? settings.privateKey : '');
+  const [region, setRegion] = useState(settings ? settings.region : '');
 
   useEffect(() => {
     if (!settings) return;
 
-    setPublicKeyPath(settings.publicKeyPath);
-    setPublicKeyPath(settings.privateKeyPath);
+    setStorageType(settings.type);
+    setPublicKey(settings.publicKey);
+    setPublicKey(settings.privateKey);
+    setRegion(settings.region);
   }, [settings]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmit(update(settings, { publicKeyPath, privateKeyPath }));
+
+    onSubmit(update(settings, {
+      type: storageType,
+      publicKey,
+      privateKey,
+      region,
+    }));
   };
 
   return (
@@ -31,22 +52,46 @@ const ServerJWTForm = (props) => {
         <FormErrorMessage error={error} />
       )}
 
-      <Field label="Public Key Path">
+      <Field label="Storage Type">
+        <Select
+          value={storageType}
+          disabled={loading}
+          onChange={setStorageType}
+          placeholder="Select storage type"
+        >
+          <Option value={storageTypes.FILE} title="File" />
+          <Option value={storageTypes.S3} title="S3" />
+        </Select>
+      </Field>
+
+      {storageType === storageTypes.S3 && (
+        <Field label="Region">
+          <Input
+            value={region}
+            autoComplete="off"
+            placeholder="Enter s3 region"
+            onValue={setRegion}
+            disabled={loading}
+          />
+        </Field>
+      )}
+
+      <Field label="Public Key" subtext={keyDescription[storageType]}>
         <Input
-          value={publicKeyPath}
+          value={publicKey}
           autoComplete="off"
-          placeholder="Enter path to public key"
-          onChange={e => setPublicKeyPath(e.target.value)}
+          placeholder="Specify public key"
+          onChange={setPublicKey}
           disabled={loading}
         />
       </Field>
 
-      <Field label="Private Key Path">
+      <Field label="Private Key" subtext={keyDescription[storageType]}>
         <Input
-          value={privateKeyPath}
+          value={privateKey}
           autoComplete="off"
-          placeholder="Enter path to private key"
-          onChange={e => setPrivateKeyPath(e.target.value)}
+          placeholder="Specify private key"
+          onValue={setPrivateKey}
           disabled={loading}
         />
       </Field>
