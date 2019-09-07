e9350e4650293f3387260827c7cdb0888756d355
dprovodnikov <dp.wireden@gmail.com>
2019-09-07 22:43:17 +0300

    Refactor <StaticFilesGeneralForm /> to useForm hook

--- src/components/screens/Management/sections/StaticFiles/StaticFilesGeneralForm.js
+++ src/components/screens/Management/sections/StaticFiles/StaticFilesGeneralForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import update from '@madappgang/update-by-path';
 import Input from '~/components/shared/Input';
 import Field from '~/components/shared/Field';
@@ -6,80 +6,75 @@ import Button from '~/components/shared/Button';
 import SaveIcon from '~/components/icons/SaveIcon';
 import LoadingIcon from '~/components/icons/LoadingIcon';
 import { Select, Option } from '~/components/shared/Select';
+import useForm from '~/hooks/useForm';
 
-const types = {
-  LOCAL: 'local',
-  S3: 's3',
-};
+const [LOCAL, S3] = ['local', 's3'];
 
 const StaticFilesGeneralForm = (props) => {
   const { loading, error, settings, onSubmit } = props;
 
-  const [type, setType] = useState(settings.type || '');
-  const [serverConfigPath, setServerConfigPath] = useState(settings.serverConfigPath || '');
-  const [staticFilesLocation, setStaticFilesLocation] = useState(settings.staticFilesLocation || '');
-  const [region, setRegion] = useState(settings.region || '');
-
-  useEffect(() => {
-    if (!settings) return;
-
-    setServerConfigPath(settings.serverConfigPath);
-    setStaticFilesLocation(settings.staticFilesLocation);
-    setType(settings.type);
-    setRegion(settings.region);
-  }, [settings]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    onSubmit(update(settings, {
-      serverConfigPath,
-      staticFilesLocation,
-      type,
-      region,
-    }));
+  const initialValues = {
+    type: settings.type || '',
+    serverConfigPath: settings.serverConfigPath || '',
+    staticFilesLocation: settings.staticFilesLocation || '',
+    region: settings.region || '',
   };
 
+  const handleSubmit = (values) => {
+    onSubmit(update(settings, values));
+  };
+
+  const form = useForm(initialValues, null, handleSubmit);
+
+  React.useEffect(() => {
+    if (!settings) return;
+
+    form.setValues(settings);
+  }, [settings]);
+
   return (
-    <form className="iap-apps-form" onSubmit={handleSubmit}>
+    <form className="iap-apps-form" onSubmit={form.handleSubmit}>
       <Field label="Storage Type">
         <Select
-          value={type}
-          onChange={setType}
+          value={form.values.type}
+          onChange={value => form.setValue('type', value)}
           placeholder="Select storage type"
         >
-          <Option value={types.LOCAL} title="Local" />
-          <Option value={types.S3} title="S3" />
+          <Option value={LOCAL} title="Local" />
+          <Option value={S3} title="S3" />
         </Select>
       </Field>
 
       <Field label="Server Config Path">
         <Input
-          value={serverConfigPath}
+          name="serverConfigPath"
+          value={form.values.serverConfigPath}
           autoComplete="off"
           placeholder="Specify path to server config"
-          onValue={setServerConfigPath}
+          onChange={form.handleChange}
           disabled={loading}
         />
       </Field>
 
       <Field label="Static Files Location">
         <Input
-          value={staticFilesLocation}
+          name="staticFilesLocation"
+          value={form.values.staticFilesLocation}
           autoComplete="off"
           placeholder="Specify path to static folder"
-          onValue={setStaticFilesLocation}
+          onChange={form.handleChange}
           disabled={loading}
         />
       </Field>
 
-      {type === types.S3 && (
+      {form.values.type === S3 && (
         <Field label="Region">
           <Input
-            value={region}
+            name="region"
+            value={form.values.region}
             autoComplete="off"
             placeholder="Specify s3 region"
-            onValue={setRegion}
+            onChange={form.handleChange}
             disabled={loading}
           />
         </Field>
