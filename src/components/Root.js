817b0499d5c1d531c4357fd9b398c2a5589b23b9
dprovodnikov <dp.wireden@gmail.com>
2019-09-10 19:16:50 +0300

    Use services context provider in Root component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginScreen from './LoginScreen';
 import ManagementScreen from './ManagementScreen';
 import NotFoundScreen from './NotFoundScreen';
 import ensureAuthState, { SIGNED_IN, SIGNED_OUT } from './ensureAuthState';
+import { ServicesContext } from '../hooks/useServices';
 
 import './Root.css';
 
-const Root = ({ store }) => (
+const Root = ({ store, services }) => (
   <Provider store={store}>
-    <BrowserRouter basename={process.env.BASE_URL}>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={ensureAuthState(SIGNED_OUT, LoginScreen, '/management')}
-        />
-        <Route
-          path="/management/:section?"
-          component={ensureAuthState(SIGNED_IN, ManagementScreen, '/')}
-        />
-        <Route component={NotFoundScreen} />
-      </Switch>
-    </BrowserRouter>
+    <ServicesContext.Provider value={services}>
+      <BrowserRouter basename={process.env.BASE_URL}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ensureAuthState(SIGNED_OUT, LoginScreen, '/management')}
+          />
+          <Route
+            path="/management/:section?"
+            component={ensureAuthState(SIGNED_IN, ManagementScreen, '/')}
+          />
+          <Route component={NotFoundScreen} />
+        </Switch>
+      </BrowserRouter>
+    </ServicesContext.Provider>
   </Provider>
 );
-
-Root.propTypes = {
-  store: PropTypes.shape().isRequired,
-};
 
 export default Root;
