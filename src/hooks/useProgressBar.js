018fbfe38a84abf449cd2c275ab29696f7b77f8f
dprovodnikov <dp.wireden@gmail.com>
2019-09-17 14:10:15 +0300

    Add an ability to use local progress state if the "local" flag is specified

--- src/hooks/useProgressBar.js
+++ src/hooks/useProgressBar.js
@@ -1,8 +1,18 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import { ProgressBarContext } from '~/components/shared/TopProgressBar';
 
-const useProgressBar = () => {
-  return useContext(ProgressBarContext);
+const useProgressBar = (options = {}) => {
+  const progressBar = useContext(ProgressBarContext);
+  const [localProgress, setLocalProgress] = useState(0);
+
+  if (options.local) {
+    return {
+      progress: localProgress,
+      setProgress: setLocalProgress,
+    };
+  }
+
+  return progressBar;
 };
 
 export default useProgressBar;
