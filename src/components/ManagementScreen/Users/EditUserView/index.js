2be7387d6d5669775ee1d33d353cb0adcd362bcd
dprovodnikov <dp.wireden@gmail.com>
2019-09-19 23:48:06 +0300

    Refactor <EditUserView /> to hooks

--- src/components/ManagementScreen/Users/EditUserView/index.js
+++ src/components/ManagementScreen/Users/EditUserView/index.js
@@ -1,168 +1,116 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import EditUserForm from './Form';
 import ActionsButton from '~/components/shared/ActionsButton';
 import {
   fetchUserById, alterUser, deleteUserById, resetUserError,
 } from '~/modules/users/actions';
-import { createNotification } from '~/modules/notifications/actions';
+import useProgressBar from '~/hooks/useProgressBar';
+import useNotifications from '~/hooks/useNotifications';
 
 const goBackPath = '/management/users';
 
-class EditUserView extends Component {
-  constructor(props) {
-    super();
+const EditUserView = ({ match, history }) => {
+  const dispatch = useDispatch();
+  const id = match.params.userid;
 
-    this.state = {};
+  const user = useSelector(s => s.selectedUser.user);
+  const error = useSelector(s => s.selectedUser.error);
 
-    this.handleCancel = this.handleCancel.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const { progress, setProgress } = useProgressBar();
+  const { notifySuccess, notifyFailure } = useNotifications();
 
-    this.availableActions = [{
-      title: 'Delete User',
-      onClick: () => props.deleteUserById(props.id),
-    }];
-  }
+  const fetchData = async () => {
+    setProgress(70);
+    await dispatch(fetchUserById(id));
+    setProgress(100);
+  };
 
-  componentDidMount() {
-    this.props.fetchUserById(this.props.id);
-  }
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    const doneSaving = prevProps.saving && !this.props.saving;
+  const handleDeleteClick = async () => {
+    setProgress(70);
 
-    if (doneSaving && !this.props.error) {
-      if (!this.props.user) {
-        this.notifyDeleteSuccess();
-      } else {
-        this.notifyCreationSuccess();
-      }
-
-      this.goBack();
+    try {
+      await dispatch(deleteUserById(id));
+      notifySuccess({
+        title: 'Deleted',
+        text: 'User has been deleted successfully',
+      });
+      history.push(goBackPath);
+    } catch (_) {
+      notifyFailure({
+        title: 'Something went wrong',
+        text: 'User could not be updated',
+      });
+    } finally {
+      setProgress(100);
     }
+  };
 
-    if (doneSaving && this.props.error) {
-      this.notifyCreationFailure();
+  const handleSubmit = async (data) => {
+    setProgress(70);
+
+    try {
+      await dispatch(alterUser(id, data));
+      notifySuccess({
+        title: 'Updated',
+        text: 'User has been updated successfully',
+      });
+      history.push(goBackPath);
+    } catch (_) {
+      notifyFailure({
+        title: 'Something went wrong',
+        text: 'User could not be updated',
+      });
+    } finally {
+      setProgress(100);
     }
-  }
+  };
 
-  notifyDeleteSuccess() {
-    this.props.createNotification({
-      type: 'success',
-      title: 'Deleted',
-      text: 'User has been deleted successfully',
-    });
-  }
+  const handleCancel = () => {
+    dispatch(resetUserError());
+    history.push(goBackPath);
+  };
 
-  notifyCreationSuccess() {
-    this.props.createNotification({
-      type: 'success',
-      title: 'Updated',
-      text: 'User has been updated successfully',
-    });
-  }
+  const availableActions = [{
+    title: 'Delete User',
+    onClick: handleDeleteClick,
+  }];
 
-  notifyCreationFailure() {
-    this.props.createNotification({
-      type: 'failure',
-      title: 'Error',
-      text: 'User could not be updated',
-    });
-  }
-
-  goBack() {
-    this.props.history.push(goBackPath);
-  }
-
-  handleCancel() {
-    this.props.resetError();
-    this.goBack();
-  }
-
-  handleSubmit(changes) {
-    this.props.alterUser(this.props.id, changes);
-  }
-
-  render() {
-    const { id, fetching, saving, user, error } = this.props;
-
-    return (
-      <section className="iap-management-section">
-        <header>
-          <div>
-            <Link to={goBackPath} className="iap-management-section__back">
-              ← &nbsp;Users
-            </Link>
-          </div>
-          <div className="iap-management-section__title">
-            User Details
-            <ActionsButton loading={fetching} actions={this.availableActions} />
-          </div>
-          <p className="iap-management-section__description">
-            <span className="iap-section-description__id">
-              id:&nbsp;
-              {id}
-            </span>
-          </p>
-        </header>
-        <main>
-          <EditUserForm
-            user={user}
-            error={error}
-            loading={fetching || saving}
-            onCancel={this.handleCancel}
-            onSubmit={this.handleSubmit}
-          />
-        </main>
-      </section>
-    );
-  }
-}
-
-EditUserView.propTypes = {
-  id: PropTypes.string.isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-  fetchUserById: PropTypes.func.isRequired,
-  alterUser: PropTypes.func.isRequired,
-  deleteUserById: PropTypes.func.isRequired,
-  resetError: PropTypes.func.isRequired,
-  fetching: PropTypes.bool,
-  saving: PropTypes.bool,
-  user: PropTypes.shape({
-    name: PropTypes.string,
-    email: PropTypes.string,
-  }),
-  error: PropTypes.instanceOf(Error),
-  createNotification: PropTypes.func.isRequired,
+  return (
+    <section className="iap-management-section">
+      <header>
+        <div>
+          <Link to={goBackPath} className="iap-management-section__back">
+            ← &nbsp;Users
+          </Link>
+        </div>
+        <div className="iap-management-section__title">
+          User Details
+          <ActionsButton loading={!!progress} actions={availableActions} />
+        </div>
+        <p className="iap-management-section__description">
+          <span className="iap-section-description__id">
+            id:&nbsp;
+            {id}
+          </span>
+        </p>
+      </header>
+      <main>
+        <EditUserForm
+          user={user}
+          error={error}
+          loading={!!progress}
+          onCancel={handleCancel}
+          onSubmit={handleSubmit}
+        />
+      </main>
+    </section>
+  );
 };
 
-EditUserView.defaultProps = {
-  fetching: false,
-  saving: false,
-  user: null,
-  error: null,
-};
-
-const mapStateToProps = (state, props) => ({
-  id: props.match.params.userid,
-  fetching: state.selectedUser.fetching,
-  saving: state.selectedUser.saving,
-  user: state.selectedUser.user,
-  error: state.selectedUser.error,
-});
-
-const actions = {
-  fetchUserById,
-  deleteUserById,
-  alterUser,
-  resetError: resetUserError,
-  createNotification,
-};
-
-export { EditUserView };
-
-export default connect(mapStateToProps, actions)(EditUserView);
+export default EditUserView;
