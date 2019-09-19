3fc24f6e6d25b33d8e3c2ebb511cc6aa89c39fc5
dprovodnikov <dp.wireden@gmail.com>
2019-09-19 16:41:56 +0300

    Refactor SearchInput to use hooks

--- src/components/shared/SearchInput/index.js
+++ src/components/shared/SearchInput/index.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Input from '~/components/shared/Input';
 import SearchIcon from '~/components/icons/SearchIcon.svg';
 import { throttle } from '~/utils/fn';
 
 import './SearchInput.css';
 
-class SearchInput extends Component {
-  constructor({ timeout }) {
-    super();
+const SearchInput = ({ placeholder, timeout, onChange }) => {
+  const [query, setQuery] = React.useState('');
 
-    this.state = {
-      query: '',
-    };
+  // for the memoized function to access
+  const queryRef = React.useRef(query);
 
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.dispatchChange = throttle(this.dispatchChange.bind(this), timeout);
-  }
+  const dispatchChange = React.useMemo(() => {
+    return throttle(() => onChange(queryRef.current), timeout);
+  }, [timeout, onChange]);
 
-  dispatchChange() {
-    this.props.onChange(this.state.query);
-  }
+  const handleSearchChange = (value) => {
+    setQuery(value);
+    queryRef.current = value;
+    dispatchChange(value);
+  };
 
-  handleSearchChange({ target }) {
-    this.setState({ query: target.value }, this.dispatchChange);
-  }
-
-  render() {
-    const { query } = this.state;
-
-    return (
-      <div className="iap-search-input">
-        <Input
-          Icon={SearchIcon}
-          value={query}
-          onChange={this.handleSearchChange}
-          placeholder={this.props.placeholder}
-        />
-      </div>
-    );
-  }
-}
-
-SearchInput.propTypes = {
-  onChange: PropTypes.func,
-  timeout: PropTypes.number,
-  placeholder: PropTypes.string,
-};
-
-SearchInput.defaultProps = {
-  timeout: 500,
-  placeholder: 'Search',
-  onChange: () => {},
+  return (
+    <div className="iap-search-input">
+      <Input
+        Icon={SearchIcon}
+        value={query}
+        onValue={handleSearchChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 
 export default SearchInput;
